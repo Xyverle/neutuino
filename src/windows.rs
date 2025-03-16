11ebcbb75495f7bab489cc604eaa7e17487cc3c5
Jacob Williams <jakedwilliams09@gmail.com>
2025-03-15 20:36:10 -0400

    make clippy happy, add docs, add asserts, etc.

--- src/windows.rs
+++ src/windows.rs
@@ -2,14 +2,6 @@
 
 use std::io;
 
-const STD_INPUT_HANDLE: u32 = 0xFFFF_FFF6;
-const STD_OUTPUT_HANDLE: u32 = 0xFFFF_FFF5;
-const ENABLE_VIRTUAL_TERMINAL_PROCESSING: u32 = 4;
-const ENABLE_ECHO_INPUT: u32 = 4;
-const ENABLE_LINE_INPUT: u32 = 2;
-const ENABLE_PROCESSED_INPUT: u32 = 1;
-const INVALID_HANDLE_VALUE: usize = usize::MAX - 1;
-
 unsafe extern "system" {
     fn GetStdHandle(nStdHandle: u32) -> usize;
     fn GetConsoleMode(hConsoleHandle: usize, dwMode: *mut u32) -> u32;
@@ -19,6 +11,14 @@ unsafe extern "system" {
         lpConsoleScreenBufferInfo: *mut ConsoleScreenBufferInfo,
     ) -> u32;
 }
+
+const STD_INPUT_HANDLE: u32 = 0xFFFF_FFF6;
+const STD_OUTPUT_HANDLE: u32 = 0xFFFF_FFF5;
+const ENABLE_VIRTUAL_TERMINAL_PROCESSING: u32 = 4;
+const ENABLE_ECHO_INPUT: u32 = 4;
+const ENABLE_LINE_INPUT: u32 = 2;
+const ENABLE_PROCESSED_INPUT: u32 = 1;
+const INVALID_HANDLE_VALUE: usize = usize::MAX - 1;
 
 #[repr(C)]
 #[derive(Default)]
@@ -36,6 +36,7 @@ struct ConsoleScreenBufferInfo {
     dwMaximumWindowSizeY: u16,
 }
 
+/// Checks if stdout is a terminal
 #[must_use]
 pub fn is_terminal() -> bool {
     let handle = get_std_handle(STD_OUTPUT_HANDLE);
@@ -48,6 +49,35 @@ pub fn is_terminal() -> bool {
     }
 }
 
+/// Enables ANSI support on Windows terminals
+///
+/// ANSI is on by default on *nix machines but still exists on them for simpler usage
+///
+/// # Errors
+///
+/// Never on *nix
+///
+/// On Windows, if There is no stdout,
+/// if stdout isn't a TTY, or
+/// if it cannot change terminal properties
+pub fn enable_ansi() -> io::Result<()> {
+    let handle = get_std_handle(STD_OUTPUT_HANDLE)?;
+    let mut dwMode = 0;
+    get_console_mode(handle, &raw mut dwMode)?;
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    set_console_mode(handle, &raw mut dwMode)?;
+    Ok(())
+}
+
+/// Gets the size of the terminal
+///
+/// Returns in (width, height) format
+///
+/// # Errors
+///
+/// If there is no stdout,
+/// if stdout isn't a TTY, or
+/// if it fails to retrieve the terminal size
 pub fn get_terminal_size() -> io::Result<(u16, u16)> {
     let handle = get_std_handle(STD_OUTPUT_HANDLE)?;
     let mut csbi = ConsoleScreenBufferInfo::default();
@@ -59,15 +89,15 @@ pub fn get_terminal_size() -> io::Result<(u16, u16)> {
     Err(io::Error::last_os_error())
 }
 
-pub fn enable_ansi() -> io::Result<()> {
-    let handle = get_std_handle(STD_OUTPUT_HANDLE)?;
-    let mut dwMode = 0;
-    get_console_mode(handle, &raw mut dwMode)?;
-    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-    set_console_mode(handle, &raw mut dwMode)?;
-    Ok(())
-}
-
+/// Enables raw mode
+///
+/// Disables input echoing, line feeding, etc.
+///
+/// # Errors
+///
+/// If there is no stdout,
+/// if stdout isn't a TTY, or
+/// if it fails to get or set terminal settings
 pub fn enable_raw_mode() -> io::Result<()> {
     let handle = get_std_handle(STD_INPUT_HANDLE)?;
     let mut dwMode = 0;
@@ -77,6 +107,15 @@ pub fn enable_raw_mode() -> io::Result<()> {
     Ok(())
 }
 
+/// Disables raw mode
+///
+/// Enables input echoing, line feeding, etc.
+///
+/// # Errors
+///
+/// If there is no stdout,
+/// if stdout isn't a TTY, or
+/// if it fails to get or set terminal settings
 pub fn disable_raw_mode() -> io::Result<()> {
     let handle = get_std_handle(STD_INPUT_HANDLE)?;
     let mut dwMode = 0;
